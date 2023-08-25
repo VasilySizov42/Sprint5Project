26aebe363d5b63fa8e39631b30475d83d5f6cabf
Vasiliy Sizov <pinky-77@yandex.ru>
2023-08-25 17:07:58 +0300

    Added AnimalFarm

---
+++ src/main/java/org/example/farm/AnimalFarm.java
@@ -0,0 +1,10 @@
+package org.example.farm;
+
+import java.util.List;
+
+public class AnimalFarm {
+    private List<String> farmAnimals;
+    public AnimalFarm(List<String> farmAnimals){
+        this.farmAnimals = farmAnimals;
+    }
+}
