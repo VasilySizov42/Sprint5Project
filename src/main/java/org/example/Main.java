a54d94093cd27d8f54e1ece63d54afc8ac49b53f
Vasiliy Sizov <pinky-77@yandex.ru>
2023-08-25 19:12:37 +0300

    Added AnimalFarm.countedAnimals()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,36 @@
 package org.example;
 
+import org.example.farm.Animal;
+import org.example.farm.AnimalFarm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> farmAnimals = Arrays.asList(
+                "CAT hgh",
+                "CAT hgh",
+                "CAT iohlulg",
+                "CAT iohlulg",
+                "CAT iohcgfvhlulg",
+                "DOG jbhig",
+                "DOG vhvyufvtu",
+                "DOG kjhbvhlvhvk",
+                "DOG jbhiuyuy",
+                "COW lknlkoih",
+                "COW lknlkoi",
+                "COW lknlk",
+                "HORSE iugygfiu",
+                "HSE iugyiu",
+                "HORSE iugyg"
+        );
+        AnimalFarm farm_1 = new AnimalFarm(farmAnimals);
+        HashMap<Animal, Integer> animalCount = farm_1.countedAnimals(farmAnimals);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.printf(String.valueOf(animalCount));
     }
 }
--- src/main/java/org/example/farm/AnimalFarm.java
+++ src/main/java/org/example/farm/AnimalFarm.java
@@ -1,10 +1,25 @@
 package org.example.farm;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class AnimalFarm {
     private List<String> farmAnimals;
     public AnimalFarm(List<String> farmAnimals){
         this.farmAnimals = farmAnimals;
+    }
+    public HashMap countedAnimals(List<String> farmAnimals){
+        HashMap<Animal, Integer> animalCount = new HashMap<>();
+        for(String animal : farmAnimals) {
+            try {
+            Animal animalType = Animal.valueOf(animal.split(" ")[0]);
+            if (!animalCount.containsKey(animalType)) animalCount.put(animalType, 1);
+             else animalCount.put(animalType, animalCount.get(animalType)+1);
+            } catch (IllegalArgumentException exception) {
+                System.out.println("Please correct string "+ animal
+                        + ". Incorrect input data.");
+            }
+        }
+        return animalCount;
     }
 }
